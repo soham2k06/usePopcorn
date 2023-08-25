01a9d4e5bd18a739795998e8737d8cc66ba46258
“soham2k06” <sohmmdev@gmail.com>
2023-08-25 19:09:59 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// function Test() {
+//   const [moviesRating, setMoviesRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating
+//         maxRating={10}
+//         color="red"
+//         size={32}
+//         onSetRating={setMoviesRating}
+//       />
+//       <p>You rated it {moviesRating} stars.</p>
+//     </div>
+//   );
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <StarRating
+      maxRating={5}
+      color={"#fcc419"}
+      size={48}
+      className={"test"}
+      messages={["Teribble", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    /> 
+    <Test /> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/useKey.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+export function useKey(key, action) {
+  useEffect(
+    function () {
+      const callback = function (e) {
+        if (e.key.toLowerCase() !== key.toLowerCase()) return;
+        action();
+      };
+      document.addEventListener("keydown", callback);
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [action]
+  );
+}
